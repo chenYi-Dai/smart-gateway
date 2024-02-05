218c6d9f1fb768983ce11cc0c006a3e031e0bd91
v_yyyiichen <v_yyyiichen@tencent.com>
2024-02-05 11:53:14 +0800

    初始化gateway工程

---
+++ src/main/java/cn/tedu/gate/beijing/SmartGateApplication.java
@@ -0,0 +1,14 @@
+package cn.tedu.gate.beijing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SmartGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGateApplication.class, args);
+    }
+
+}
